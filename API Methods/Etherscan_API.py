1c92071168789eb2b96e4923d60eb6aa4c3eae8f
Parker Burchett <parkerburchett@gmail.com>
2021-01-12 10:45:54 -0800

    Added etherscan.io API key to git_ignore

---
+++ API Methods/Etherscan_API.py
@@ -0,0 +1,6 @@
+# https://info.etherscan.com/api-return-errors/
+
+# limited by 5 calls / second per ip with a valid API key. They keys are publicly are free.
+
+def get_normal_transactions(miner_address)
+
--- account_transaction_parser.py
+++ account_transaction_parser.py
@@ -1,37 +1,5 @@
 """
-This takes a .json object from the API and writes it out neatly to a .csv file
-
-https://etherscan.io/address/0xCeB4d0CA821420Cf2553b9e244F6B52364613F94
-This is ethan's miner.
-
-Do this since you knwo what the values should be
-
-https://etherscan.io/apis
-
-The command to get the json file from the api is
-
-
-https://api.etherscan.io/api?module=account&action=txlist&address=0xCeB4d0CA821420Cf2553b9e244F6B52364613F94&startblock=0&endblock=99999999&sort=asc&apikey=3KKNT8U3S4Q44UUVFNCD2DSUUBF8H29J5F
-
-
-This ought to be the same as
-https://etherscan.io/address/0xCeB4d0CA821420Cf2553b9e244F6B52364613F94
-
-For whatever reason the 'value' dict object is different than the values
-I know they point at the same account
-
-Another option
-https://api.etherscan.io/api?module=account&action=txlist&address=0xCeB4d0CA821420Cf2553b9e244F6B52364613F94&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=3KKNT8U3S4Q44UUVFNCD2DSUUBF8H29J5F
-
-
-This is not working and I don't know why
-
-
-https://ethplorer.io/
-
-
-
-"""
+""
 
 
 # proper command https://api.ethplorer.io/getAddressTransactions/0xb297cacf0f91c86dd9d2fb47c6d12783121ab780?apiKey=freekey

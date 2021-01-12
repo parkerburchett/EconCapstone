297a126d1feccb3be81fcb772880a27a769971ed
Parker Burchett <parkerburchett@gmail.com>
2021-01-12 10:31:41 -0800

    Moved common methods to their own file.

---
+++ utils/Data_Cleaning.py
@@ -0,0 +1,19 @@
+"""
+    This is where I am keeping all the data parsing methods.
+"""
+import math
+import datetime
+
+def convert_wei_to_ether(amount_wei):
+    amount_ether = float(amount_wei) / math.pow(10, 18) # there are 10^18 wei in an ether.
+    return amount_ether
+
+def convert_unix_to_standard_date(unix_time):
+    standard_date = datetime.datetime.fromtimestamp(unix_time).date() # I only care about the date. I am choosing to ingore data about the time
+    return standard_date
+
+def convert_hashes_to_mega_hashes(hashes):
+    mega_hashes = float(hashes) / math.pow(10, 6) # there are a million 10^6 hashes in a mega hash
+    return  mega_hashes
+
+

19df757d30c93ff0de4cfd050c537609e42fe674
Parker Burchett <parkerburchett@gmail.com>
2021-01-12 10:31:10 -0800

    Added method to clean worker data from etherscan

--- Ethermine API interface/Commands_for_Etherscan_API.py
+++ Ethermine API interface/Commands_for_Etherscan_API.py
@@ -3,6 +3,7 @@ import requests
 import json
 import math
 from datetime import datetime
+from utils import Data_Cleaning
 
 
 
@@ -15,6 +16,9 @@ from datetime import datetime
 # 10&9 Gwei = 1 ether
 
 # to go from base units (Wei into ether * 10^18)
+
+
+
 
 def get_payouts_command(miner_address):
     """
@@ -50,17 +54,35 @@ def convert_payouts_into_human_readable(payouts):
     """
     simple_payments =[]
     for p in payouts:
-        amount_ether = float(p['amount']) / math.pow(10,18) # go from Wei to ether
-        payment_date = datetime.fromtimestamp(p['paidOn']).date() # go from unix time stamp to a date
+        amount_ether = Data_Cleaning.convert_wei_to_ether(p['amount'])
+        payment_date = Data_Cleaning.convert_unix_to_standard_date(p['paidOn'])
         simple_payments.append((payment_date,amount_ether))
 
     return simple_payments # (payment date, amount in ether) tuple
+
+def parse_worker_data(workers):
+    """
+        Converts the list of workers to an array of tuples of 'workerID, average(mH/s), date queried)
+    :param workers: a list of worker objects from the etherscan.org API
+    :return: a list of (worker_id, avg_Mega_Hashes_per_second, date queried)
+    """
+    simplified_worker_data =[]
+    # this will happen many times, you might large might want to rewrite as list comprehension.
+    # I think that the primary limit will be the number of API calls I can make. Not the time cost of the the data.
+    for w in workers:
+        worker_id = w['worker']
+        date_queried= Data_Cleaning.convert_unix_to_standard_date(w['time'])
+        avg_Mega_Hashes_per_second = Data_Cleaning.convert_hashes_to_mega_hashes(w['averageHashrate'])
+        worker_stats = (worker_id,avg_Mega_Hashes_per_second,date_queried)
+        simplified_worker_data.append(worker_stats)
+
+    return simplified_worker_data
 
 
 
 ethan ='CeB4d0CA821420Cf2553b9e244F6B52364613F94'
 big_random_miner = '969aE8B1708E825570a1bBF4C9C7D2FC7382BadD'
-#
+
 # command = get_payouts_command(big_random_miner)
 # my_data =get_data_from_command(command)
 # simple_payments = convert_payouts_into_human_readable(my_data)
@@ -69,5 +91,13 @@ big_random_miner = '969aE8B1708E825570a1bBF4C9C7D2FC7382BadD'
 w_command = get_workers_command(big_random_miner)
 list_of_workers =get_data_from_command(w_command)
 
-# {'worker': '0030180db667', 'time': 1610412600, 'lastSeen': 1610412561, 'reportedHashrate': 243666806, 'currentHashrate': 208833333.33333334, 'validShares': 186, 'invalidShares': 0, 'staleShares': 3, 'averageHashrate': 241443672.83950615}
+worker_data = parse_worker_data(list_of_workers)
+
+
+for w in worker_data[:50]:
+    print(w)
+
+
+
+
 print('fin')
